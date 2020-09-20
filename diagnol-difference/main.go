956ef5ccee105bb1d7daed3a4984c448e4827bdc
Nickolas Gough <nickolas.v.gough@gmail.com>
2020-09-20 15:18:34 -0600

    Complete the Diagnoal Difference challenge.

---
+++ diagnol-difference/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the 'diagonalDifference' function below.
+// Found at https://www.hackerrank.com/challenges/diagonal-difference/problem.
+func diagonalDifference(matrix [][]int32) int32 {
+	var lSum, rSum int32
+	lIndex := int32(0)
+	rIndex := len(matrix) - 1
+	for _, row := range matrix {
+		lSum += row[lIndex]
+		rSum += row[rIndex]
+		lIndex++
+		rIndex--
+	}
+	return int32(math.Abs(float64(lSum - rSum)))
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	var arr [][]int32
+	for i := 0; i < int(n); i++ {
+		arrRowTemp := strings.Split(strings.TrimRight(readLine(reader)," \t\r\n"), " ")
+
+		var arrRow []int32
+		for _, arrRowItem := range arrRowTemp {
+			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arrRow = append(arrRow, arrItem)
+		}
+
+		if len(arrRow) != int(n) {
+			panic("Bad input")
+		}
+
+		arr = append(arr, arrRow)
+	}
+
+	result := diagonalDifference(arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
